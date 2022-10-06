f4a06f9f5a7de9849258195ae48fc4379df47da7
divyav-aot <divyav-aot@github>
2022-10-06 12:13:03 -0400

    initial commit

---
+++ web/src/constants/constants.tsx
@@ -0,0 +1,12 @@
+
+//keycloak
+export const Keycloak_Client = "foi-document-redaction";
+export const KEYCLOAK_REALM = "5k8dbl4h";
+export const KEYCLOAK_URL ="https://dev.oidc.gov.bc.ca";
+export const KEYCLOAK_AUTH_URL = `${KEYCLOAK_URL}/auth`;
+export const ANONYMOUS_USER = "anonymous";
+export const SESSION_SECURITY_KEY = "u7x!A%D*G-KaNdRgUkXp2s5v8y/B?E(H";
+//6 hour in milliseconds
+export const SESSION_LIFETIME = 21600000;
+
+
---
+++ web/src/services/StoreService.ts
@@ -0,0 +1,28 @@
+import thunk from "redux-thunk";
+import {createBrowserHistory} from "history";
+import { createReduxHistoryContext } from "redux-first-history";
+import {combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import user from "../modules/userDetailReducer";
+
+const {
+  createReduxHistory,
+  routerMiddleware,
+  routerReducer
+} = createReduxHistoryContext({ history: createBrowserHistory() });
+
+export const store = configureStore({
+  reducer: combineReducers({
+    user,
+    router: routerReducer
+  }),
+  middleware: [thunk, routerMiddleware]
+});
+
+export const history = createReduxHistory(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
