620253827ffb3987b1e572f2f47ac76dfc54b58c
divyav-aot <divyav-aot@github>
2022-10-07 14:42:09 -0400

    fixed references

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {store, history} from "./services/storeService";
+import {store, history} from "./services/StoreService";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
