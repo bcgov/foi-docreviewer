7d51da531ffe33e0d82fc64ebe32b9183cf2e7a1
Richard Qi <richard.qi@aot-technologies.com>
2022-10-11 12:03:19 -0700

    added one revision for migration

---
+++ api/migrations/versions/b96f3a390985_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b96f3a390985
+Revises: 
+Create Date: 2022-10-11 12:02:04.313388
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b96f3a390985'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
