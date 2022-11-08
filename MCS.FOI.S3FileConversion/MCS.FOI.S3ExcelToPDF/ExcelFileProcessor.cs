1784637f2e86dd70d5a74defca956f4c2cfb4565
unknown <nicholas.kan@aot-technologies.com>
2022-11-08 10:19:35 -0800

    initial commit for file conversion .net solution

---
+++ MCS.FOI.S3FileConversion/MCS.FOI.S3ExcelToPDF/ExcelFileProcessor.cs
@@ -0,0 +1,213 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIORenderer;
+using System;
+using System.IO;
+using System.Threading;
+using Serilog;
+
+namespace MCS.FOI.ExcelToPDF
+{
+    /// <summary>
+    /// Excel File Processor to convert XLS, XLSX to PDF, based on the Synfusion Libraries.
+    /// </summary>
+    public class ExcelFileProcessor : IExcelFileProcessor
+    {
+
+        public ExcelFileProcessor() { }
+
+        /// <summary>
+        /// Overloaded Constructor to recieve the Source Excel location and output location to save the PDF.
+        /// </summary>
+        /// <param name="sourceExcelFilePath"></param>
+        /// <param name="outputPdfFilePath"></param>
+        /// <param name="excelFileName"></param>
+        public ExcelFileProcessor(Stream sourceStream, string outputPdfFilePath)
+        {
+            this.SourceStream = sourceStream;
+            //this.ExcelSourceFilePath = sourceExcelFilePath;
+            this.PdfOutputFilePath = outputPdfFilePath;
+            //this.ExcelFileName = excelFileName;
+        }
+
+        /// <summary>
+        /// Source Stream from S3 response
+        /// </summary>
+        public Stream SourceStream { get; set; }
+
+        /// <summary>
+        /// Source Excel Path, this will be full path including sub folders/ directories
+        /// </summary>
+        public string ExcelSourceFilePath { get; set; }
+
+        /// <summary>
+        /// PDF output path with sub folder(s) path
+        /// </summary>
+        public string PdfOutputFilePath { get; set; }
+
+        /// <summary>
+        /// Source File Name
+        /// </summary>
+        public string ExcelFileName { get; set; }
+
+        /// <summary>
+        /// Flag to indicate to Syncfusion Xls to PDF Conversion, whether its a single page output for all spreadsheets
+        /// </summary>
+        public bool IsSinglePDFOutput { get; set; }
+
+        /// <summary>
+        /// Counts / tries to file convert , if that file already under access or updates or copying is still in progress
+        /// </summary>
+        public int FailureAttemptCount { get; set; }
+
+        /// <summary>
+        /// Wait in Milli seconds before trying next attempt
+        /// </summary>
+        public int WaitTimeinMilliSeconds { get; set; }
+
+       /// <summary>
+       /// Main Conversion Method, including Sysnfusion components, Failure recovery attempts and PDF conversion
+       /// </summary>
+       /// <returns>return a tuple wwith file conversion status.</returns>
+        public (bool, string, string, Stream) ConvertToPDF()
+        {
+            bool converted = false;
+            string message = string.Empty;
+            bool _isSinglePDFOutput = IsSinglePDFOutput;
+            MemoryStream output = new MemoryStream();
+            try
+            {
+                //string sourceFile = Path.Combine(ExcelSourceFilePath, ExcelFileName);
+                //if (File.Exists(sourceFile))
+                //{
+                    using (ExcelEngine excelEngine = new ExcelEngine())
+                    {
+                        IApplication application = excelEngine.Excel;
+
+                        var excelparseoptions = ExcelParseOptions.DoNotParsePivotTable;
+
+                        for (int attempt = 1; attempt < FailureAttemptCount; attempt++)
+                        {
+                            Stream excelStream;
+                            
+                            try
+                            {
+                                //using (excelStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read))
+                                using (excelStream = SourceStream)
+                                {
+                                    
+                                    IWorkbook workbook = application.Workbooks.Open(excelStream, excelparseoptions);
+
+                                    if (workbook.Worksheets.Count > 0)
+                                    {
+                                        if (!_isSinglePDFOutput) /// if not single output, then traverse through each sheet and make seperate o/p pdfs
+                                        {
+                                            foreach (IWorksheet worksheet in workbook.Worksheets)
+                                            {
+                                                if (worksheet.Visibility == WorksheetVisibility.Visible)
+                                                {                                                   
+                                                    //worksheet.UsedRange.AutofitRows();                                                    
+                                                    output = saveToPdf(worksheet, output);
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            output = saveToPdf(workbook, output);
+
+                                        }
+                                    }
+
+                                    converted = true;
+                                    message = $"File processed successfully!";
+                                    break;
+                                }
+                            }
+                            catch(Exception e)
+                            {
+                                message = $"Exception happened while accessing File, re-attempting count : {attempt} , Error Message : {e.Message} , Stack trace : {e.StackTrace}";
+                                Log.Error(message);
+                                Console.WriteLine(message);
+                                excelStream = null;
+                                Thread.Sleep(WaitTimeinMilliSeconds);
+                                if(attempt >= 3 && attempt < 5 && attempt < FailureAttemptCount)
+                                {
+                                    excelparseoptions = ExcelParseOptions.ParseWorksheetsOnDemand;
+                                }                                
+                                else
+                                {
+                                    excelparseoptions = ExcelParseOptions.DoNotParsePivotTable;
+                                }
+                            }
+                        }
+
+                    }
+                //}
+                //else
+                //{
+                //    message = $"{sourceFile} does not exist!";
+                //    Log.Error(message);
+                //    //return converted;
+                //}
+            }
+            catch (Exception ex)
+            {
+                converted = false;
+                string error = $"Exception occured while coverting file at {ExcelSourceFilePath} , exception :  {ex.Message} , stacktrace : {ex.StackTrace}";
+                Log.Error(error);
+                Console.WriteLine(error);
+            }
+
+            return (converted, message, PdfOutputFilePath, output);
+        }
+
+       
+        /// <summary>
+        /// Save to pdf method, based on input from Excel file - Workbook vs Worksheet
+        /// </summary>
+        /// <param name="worksheet">worksheet object</param>
+        private MemoryStream saveToPdf(IWorksheet worksheet, MemoryStream output)
+        {
+            XlsIORenderer renderer = new XlsIORenderer();
+            
+            if (!Directory.Exists(PdfOutputFilePath))
+                Directory.CreateDirectory(PdfOutputFilePath);
+
+            string _worksheetName = FileNameUtil.GetFormattedFileName(worksheet.Name); ;
+       
+            string outputFileName = Path.Combine(PdfOutputFilePath, $"{Path.GetFileNameWithoutExtension(ExcelFileName)}_{_worksheetName}");
+                      
+            using var pdfDocument = renderer.ConvertToPDF(worksheet, new XlsIORendererSettings() { LayoutOptions = LayoutOptions.FitAllColumnsOnOnePage });
+            using var stream = new FileStream($"{outputFileName}.pdf", FileMode.Create, FileAccess.Write); 
+            pdfDocument.PageSettings.Margins = new Syncfusion.Pdf.Graphics.PdfMargins() { All = 10 };
+            pdfDocument.Compression = PdfCompressionLevel.Normal;            
+            pdfDocument.Save(stream);
+            pdfDocument.Save(output);
+            stream.Dispose();
+            return output;
+
+        }
+
+        /// <summary>
+        ///  Save to pdf method, based on input from Excel file - Workbook vs Worksheet
+        /// </summary>
+        /// <param name="workbook">workbook object</param>
+        private MemoryStream saveToPdf(IWorkbook workbook, MemoryStream output)
+        {
+            XlsIORenderer renderer = new XlsIORenderer();
+            if (!Directory.Exists(PdfOutputFilePath))
+                Directory.CreateDirectory(PdfOutputFilePath);
+            string outputFileName = Path.Combine(PdfOutputFilePath, $"{Path.GetFileNameWithoutExtension(ExcelFileName)}.pdf");
+            PdfDocument pdfDocument = renderer.ConvertToPDF(workbook, new XlsIORendererSettings() { LayoutOptions = LayoutOptions.FitAllColumnsOnOnePage });
+            Stream stream = new FileStream(outputFileName, FileMode.Create, FileAccess.ReadWrite);
+            pdfDocument.Save(stream);
+            pdfDocument.Save(output);
+            stream.Dispose();
+            return output;
+
+        }
+
+
+
+    }
+}
---
+++ MCS.FOI.S3FileConversion/MCS.FOI.S3FileConversion/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+//using Serilog;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using System.Linq;
+using System.Net.Http;
+using Amazon;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using MCS.FOI.ExcelToPDF;
+
+namespace MCS.FOI.S3FileConversion
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            //string s3FilePath = args[0];
+            Console.WriteLine("Hello World");
+
+            var configurationbuilder = new ConfigurationBuilder()
+                        .AddJsonFile($"appsettings.json", true, true)
+                        //.AddJsonFile($"appsettings.{environmentName}.json", true, true) // TEMP: Commented out, will track back when we migrate to Cloud/OS in future
+                        .AddEnvironmentVariables().Build();
+
+            //Log.Logger = new LoggerConfiguration()
+            //.ReadFrom.Configuration(configurationbuilder)
+            //.CreateLogger();
+
+
+            string S3AccessKeyID = configurationbuilder.GetSection("ConversionSettings:S3AccessKeyID").Value;
+            string S3AccessSecretKey = configurationbuilder.GetSection("ConversionSettings:S3AccessSecretKey").Value;
+            string S3Bucket = configurationbuilder.GetSection("ConversionSettings:S3Bucket").Value;
+            string S3ServiceAccount = configurationbuilder.GetSection("ConversionSettings:S3ServiceAccount").Value;
+            string S3Host = configurationbuilder.GetSection("ConversionSettings:S3Host").Value;
+
+            IAmazonS3 s3;
+
+
+            AWSCredentials AWSCredentials = new BasicAWSCredentials("access key", "secret access key");
+            AmazonS3Config config = new AmazonS3Config
+            {
+                //USEast1RegionalEndpointValue = Amazon.Runtime.S3UsEast1RegionalEndpointValue.Regional,
+
+                ServiceURL = "https://citz-foi-prod.objectstore.gov.bc.ca"
+                //RegionEndpoint = RegionEndpoint.USEast1,
+                //ServiceURL = S3Host,
+            };
+            //config.SignatureVersion = "false";
+            //config.SignatureVersion = "AWS4-HMAC-SHA256";
+
+            AmazonS3Client s3Client = new AmazonS3Client(AWSCredentials, config);
+
+            using (s3 = new AmazonS3Client(AWSCredentials, config))
+            {
+                //var list = await s3.ListBucketsAsync();
+                //var buckets = list.Buckets.Select(x => x.BucketName);
+
+                //Console.WriteLine(buckets.Count());
+
+                ListBucketsResponse listresponse = await s3Client.ListBucketsAsync();
+                foreach (S3Bucket b in listresponse.Buckets)
+                {
+                    Console.WriteLine("{0}\t{1}", b.BucketName, b.CreationDate);
+                }
+
+                var presignedGetURL = GetPresignedURL(s3, "edu-dev/test1.xlsx", HttpVerb.GET);
+                Console.WriteLine(presignedGetURL);
+
+                var client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(presignedGetURL); 
+                response.EnsureSuccessStatusCode();
+                //string responseBody = await response.Content.ReadAsStringAsync();
+                Stream responseStream = response.Content.ReadAsStream();
+                ExcelFileProcessor excelFileProcessor = new ExcelFileProcessor(responseStream, "C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx");
+                excelFileProcessor.IsSinglePDFOutput = false;
+                excelFileProcessor.WaitTimeinMilliSeconds = 5000;
+                excelFileProcessor.FailureAttemptCount = 10;
+                var (converted, message, PdfOutputFilePath, output) = excelFileProcessor.ConvertToPDF();
+
+                var presignedPutURL = GetPresignedURL(s3, "edu-dev/test1.pdf", HttpVerb.PUT);
+
+                output.Position = 0;
+                StreamContent strm = new StreamContent(output);
+                //StreamContent strm = new StreamContent(new FileStream("C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx\\_Sheet1.pdf", FileMode.Open, FileAccess.Read));
+                strm.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                HttpResponseMessage putRespMsg = await client.PutAsync(presignedPutURL, strm);
+
+                //Console.WriteLine(responseBody);
+
+                //listobjectsv2request request = new listobjectsv2request();
+                //request.bucketname = "/edu-dev";
+                //listobjectsv2response response = await s3client.listobjectsv2async(request);
+                //foreach (s3object o in response.s3objects)
+                //{
+                //    console.writeline("{0}\t{1}\t{2}", o.key, o.size, o.lastmodified);
+                //}
+
+                //var listReq = new ListObjectsRequest
+                //{
+                //    BucketName = "/dev-forms-foirequests"
+                //};
+
+                //var listResponse = await s3Client.ListObjectsAsync(listReq);
+
+                //foreach (S3Object entry in listResponse.S3Objects)
+                //{
+                //    Console.Write(entry.Key);
+                //}
+
+                //var filePath = S3Host + "/dev-forms-foirequests/test1.xlsx";
+
+                //GetObjectRequest request = new GetObjectRequest
+                //{
+                //    BucketName = "edu-dev",
+                //    Key = "edu-dev/test1.xlsx"
+                //};
+                //request.ResponseHeaderOverrides.CacheControl = "no-cache, max-age=0";
+
+                ////try
+                ////{
+                //using GetObjectResponse response = await s3.GetObjectAsync(request);
+                //await response.WriteResponseStreamToFileAsync("C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx", true, System.Threading.CancellationToken.None);
+            }
+            //}
+            //catch (AmazonS3Exception ex)
+            //{
+            //    Console.WriteLine($"Error encountered on server. Message:'{ex.Message}' getting list of objects.");
+            //}
+
+        }
+        private static string GetPresignedURL(IAmazonS3 s3, string fileName, HttpVerb method)
+        {
+            AWSConfigsS3.UseSignatureVersion4 = true;
+            GetPreSignedUrlRequest request1 = new()
+            {
+                Key = fileName,
+                Verb = method,
+                //ContentType = "application/pdf",
+                Expires = DateTime.Now.AddHours(1),
+                Protocol = Protocol.HTTPS,
+                //ServerSideEncryptionKeyManagementServiceKeyId = S3AccessSecretKey,
+                //ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256
+            };
+            //request1.ResponseHeaderOverrides.ContentType = "application/df";
+            return s3.GetPreSignedURL(request1);
+        }
+    }
+}
