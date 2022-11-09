119c1fa739ae086d7bb970145bf908d8005fd7b4
Richard Qi <richard.qi@aot-technologies.com>
2022-11-08 22:12:54 -0800

    fix user loading issue

--- web/src/components/FOI/Home/Home.tsx
+++ web/src/components/FOI/Home/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
           <DocumentSelector setCurrentPage={setCurrentPage}/>
         </Grid>
         <Grid item xs={true}>
-          <Redlining currentPage={currentPage} user={user}/>
+          { (user?.name || user?.preferred_username) ? <Redlining currentPage={currentPage} user={user}/> : <div>Loading</div> }
           {/* <header className="app-header">
             <span className="navbar-text" style={{}}> Hi {user.name || user.preferred_username || ""}</span>
           </header> */}
