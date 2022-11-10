92e69b7a97b76f197fb9bc541f09253869d8411b
unknown <nicholas.kan@aot-technologies.com>
2022-11-10 10:02:38 -0800

    create separate s3 handler file add integration with redis streams remove unnecessary files from git

--- MCS.FOI.S3FileConversion/MCS.FOI.S3FileConversion/Program.cs
+++ MCS.FOI.S3FileConversion/MCS.FOI.S3FileConversion/Program.cs
@@ -2,16 +2,16 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
 //using Serilog;
-using Amazon.S3;
-using Amazon.S3.Model;
-using Amazon.Runtime;
 using System.Linq;
 using System.Net.Http;
 using Amazon;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using MCS.FOI.ExcelToPDF;
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+using SkiaSharp;
 
 namespace MCS.FOI.S3FileConversion
 {
@@ -27,127 +27,64 @@ namespace MCS.FOI.S3FileConversion
                         //.AddJsonFile($"appsettings.{environmentName}.json", true, true) // TEMP: Commented out, will track back when we migrate to Cloud/OS in future
                         .AddEnvironmentVariables().Build();
 
+
+
             //Log.Logger = new LoggerConfiguration()
             //.ReadFrom.Configuration(configurationbuilder)
             //.CreateLogger();
 
 
-            string S3AccessKeyID = configurationbuilder.GetSection("ConversionSettings:S3AccessKeyID").Value;
-            string S3AccessSecretKey = configurationbuilder.GetSection("ConversionSettings:S3AccessSecretKey").Value;
-            string S3Bucket = configurationbuilder.GetSection("ConversionSettings:S3Bucket").Value;
-            string S3ServiceAccount = configurationbuilder.GetSection("ConversionSettings:S3ServiceAccount").Value;
-            string S3Host = configurationbuilder.GetSection("ConversionSettings:S3Host").Value;
+            //string S3Host = configurationbuilder.GetSection("ConversionSettings:S3Host").Value;
 
-            IAmazonS3 s3;
+            
 
 
-            AWSCredentials AWSCredentials = new BasicAWSCredentials("access key", "secret access key");
-            AmazonS3Config config = new AmazonS3Config
+            string eventHubHost = configurationbuilder.GetSection("EventHub:Host").Value;
+            string eventHubPort = configurationbuilder.GetSection("EventHub:Port").Value;
+            //string eventHubPort = configurationbuilder.GetSection("EventHub:Port").Value;
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
             {
-                //USEast1RegionalEndpointValue = Amazon.Runtime.S3UsEast1RegionalEndpointValue.Regional,
+                EndPoints = { $"{eventHubHost}:{eventHubPort}" },
+                Password = configurationbuilder.GetSection("EventHub:Password").Value
+            });
 
-                ServiceURL = "https://citz-foi-prod.objectstore.gov.bc.ca"
-                //RegionEndpoint = RegionEndpoint.USEast1,
-                //ServiceURL = S3Host,
-            };
-            //config.SignatureVersion = "false";
-            //config.SignatureVersion = "AWS4-HMAC-SHA256";
+            var db = redis.GetDatabase();
+            var pong = await db.PingAsync();
+            Console.WriteLine(pong);
 
-            AmazonS3Client s3Client = new AmazonS3Client(AWSCredentials, config);
+            //string latest = "1667951169543-0";
+            string latest = "$";
+            string streamKey = configurationbuilder.GetSection("EventHub:StreamKey").Value;
+            //db.StreamCreateConsumerGroup(streamKey, "file-conversion-consumer-group", "$");
 
-            using (s3 = new AmazonS3Client(AWSCredentials, config))
+            while (true)
             {
-                //var list = await s3.ListBucketsAsync();
-                //var buckets = list.Buckets.Select(x => x.BucketName);
-
-                //Console.WriteLine(buckets.Count());
-
-                ListBucketsResponse listresponse = await s3Client.ListBucketsAsync();
-                foreach (S3Bucket b in listresponse.Buckets)
-                {
-                    Console.WriteLine("{0}\t{1}", b.BucketName, b.CreationDate);
+                var messages = db.StreamReadGroup(streamKey, "file-conversion-consumer-group", "c1");
+                if (messages.Length > 0) {
+                    foreach (StreamEntry message in messages)
+                    {
+                        //Console.WriteLine(message);
+                        Console.WriteLine("Message ID: {0} Converting: {1}", message.Id, message["S3Path"]);
+                        await S3Handler.convertFile(message["S3Path"]);
+                        latest = message.Id;
+                        db.StringSet($"{latest}:lastid", latest);
+                        db.StreamAcknowledge(streamKey, "file-conversion-consumer-group", message.Id);
+                        Console.WriteLine("Finished Converting: {0}", message["S3Path"]); 
+                    }
+                } else {
+                    Console.WriteLine("No new messages after {0}", latest);
                 }
-
-                var presignedGetURL = GetPresignedURL(s3, "edu-dev/test1.xlsx", HttpVerb.GET);
-                Console.WriteLine(presignedGetURL);
-
-                var client = new HttpClient();
-                HttpResponseMessage response = await client.GetAsync(presignedGetURL); 
-                response.EnsureSuccessStatusCode();
-                //string responseBody = await response.Content.ReadAsStringAsync();
-                Stream responseStream = response.Content.ReadAsStream();
-                ExcelFileProcessor excelFileProcessor = new ExcelFileProcessor(responseStream, "C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx");
-                excelFileProcessor.IsSinglePDFOutput = false;
-                excelFileProcessor.WaitTimeinMilliSeconds = 5000;
-                excelFileProcessor.FailureAttemptCount = 10;
-                var (converted, message, PdfOutputFilePath, output) = excelFileProcessor.ConvertToPDF();
-
-                var presignedPutURL = GetPresignedURL(s3, "edu-dev/test1.pdf", HttpVerb.PUT);
-
-                output.Position = 0;
-                StreamContent strm = new StreamContent(output);
-                //StreamContent strm = new StreamContent(new FileStream("C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx\\_Sheet1.pdf", FileMode.Open, FileAccess.Read));
-                strm.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-                HttpResponseMessage putRespMsg = await client.PutAsync(presignedPutURL, strm);
-
-                //Console.WriteLine(responseBody);
-
-                //listobjectsv2request request = new listobjectsv2request();
-                //request.bucketname = "/edu-dev";
-                //listobjectsv2response response = await s3client.listobjectsv2async(request);
-                //foreach (s3object o in response.s3objects)
-                //{
-                //    console.writeline("{0}\t{1}\t{2}", o.key, o.size, o.lastmodified);
-                //}
-
-                //var listReq = new ListObjectsRequest
-                //{
-                //    BucketName = "/dev-forms-foirequests"
-                //};
-
-                //var listResponse = await s3Client.ListObjectsAsync(listReq);
-
-                //foreach (S3Object entry in listResponse.S3Objects)
-                //{
-                //    Console.Write(entry.Key);
-                //}
-
-                //var filePath = S3Host + "/dev-forms-foirequests/test1.xlsx";
-
-                //GetObjectRequest request = new GetObjectRequest
-                //{
-                //    BucketName = "edu-dev",
-                //    Key = "edu-dev/test1.xlsx"
-                //};
-                //request.ResponseHeaderOverrides.CacheControl = "no-cache, max-age=0";
-
-                ////try
-                ////{
-                //using GetObjectResponse response = await s3.GetObjectAsync(request);
-                //await response.WriteResponseStreamToFileAsync("C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx", true, System.Threading.CancellationToken.None);
+                Thread.Sleep(6000);
             }
-            //}
-            //catch (AmazonS3Exception ex)
-            //{
-            //    Console.WriteLine($"Error encountered on server. Message:'{ex.Message}' getting list of objects.");
-            //}
+
+
+            
 
         }
-        private static string GetPresignedURL(IAmazonS3 s3, string fileName, HttpVerb method)
-        {
-            AWSConfigsS3.UseSignatureVersion4 = true;
-            GetPreSignedUrlRequest request1 = new()
-            {
-                Key = fileName,
-                Verb = method,
-                //ContentType = "application/pdf",
-                Expires = DateTime.Now.AddHours(1),
-                Protocol = Protocol.HTTPS,
-                //ServerSideEncryptionKeyManagementServiceKeyId = S3AccessSecretKey,
-                //ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256
-            };
-            //request1.ResponseHeaderOverrides.ContentType = "application/df";
-            return s3.GetPreSignedURL(request1);
-        }
+
+
+
+        
     }
 }
---
+++ MCS.FOI.S3FileConversion/MCS.FOI.S3FileConversion/S3Handler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using Microsoft.Extensions.Configuration;
+using Amazon;
+using MCS.FOI.ExcelToPDF;
+
+namespace MCS.FOI.S3FileConversion
+{
+    internal class S3Handler
+    {
+        public static async System.Threading.Tasks.Task convertFile(string filePath)
+        {
+            // Get S3 Access credentials based on ministry
+            var cb = new ConfigurationBuilder().AddJsonFile($"s3access.json", true, true).AddEnvironmentVariables().Build();
+            string S3Host = cb.GetSection("S3Host").Value;
+            string bucket = filePath.Split("/")[3];
+            string S3AccessKeyID = cb.GetSection($"AccountMapping:{bucket}:S3AccessKeyID").Value;
+            string S3AccessSecretKey = cb.GetSection($"AccountMapping:{bucket}:S3AccessSecretKey").Value;
+            string S3ServiceAccount = cb.GetSection($"AccountMapping:{bucket}:S3ServiceAccount").Value;
+
+            try {
+                // Initialize S3 Client
+                IAmazonS3 s3;
+                AWSCredentials AWSCredentials = new BasicAWSCredentials(S3AccessKeyID, S3AccessSecretKey);
+                AmazonS3Config config = new AmazonS3Config
+                {
+                    ServiceURL = S3Host
+                };
+
+                AmazonS3Client s3Client = new AmazonS3Client(AWSCredentials, config);
+
+                using (s3 = new AmazonS3Client(AWSCredentials, config))
+                {
+                    // Get File from s3
+                    var fileKey = filePath.Split(S3Host + '/')[1];
+                    var presignedGetURL = GetPresignedURL(s3, fileKey, HttpVerb.GET);
+
+                    var client = new HttpClient();
+                    HttpResponseMessage response = await client.GetAsync(presignedGetURL);
+                    response.EnsureSuccessStatusCode();
+                    Stream responseStream = response.Content.ReadAsStream();
+
+                    // Convert File
+                    string extension = Path.GetExtension(fileKey);
+                    Stream output = new MemoryStream();
+                    switch (extension)
+                    {
+                        case ".xls":
+                        case ".xlsx":
+                            output = convertExcelFiles(responseStream);
+                            break;
+                    }
+
+
+                    // Save converted pdf back to s3
+                    var newKey = Path.ChangeExtension(fileKey, ".pdf");
+                    var presignedPutURL = GetPresignedURL(s3, newKey, HttpVerb.PUT);
+
+                    output.Position = 0;
+                    StreamContent strm = new StreamContent(output);
+                    strm.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                    HttpResponseMessage putRespMsg = await client.PutAsync(presignedPutURL, strm);
+
+                }
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error encountered on server. Message:'{ex.Message}' getting list of objects.");
+            }
+        }
+
+        public static string GetPresignedURL(IAmazonS3 s3, string fileName, HttpVerb method)
+        {
+            AWSConfigsS3.UseSignatureVersion4 = true;
+            GetPreSignedUrlRequest request = new()
+            {
+                Key = fileName,
+                Verb = method,
+                //ContentType = "application/pdf",
+                Expires = DateTime.Now.AddHours(1),
+                Protocol = Protocol.HTTPS,
+                //ServerSideEncryptionKeyManagementServiceKeyId = S3AccessSecretKey,
+                //ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256
+            };
+            //request1.ResponseHeaderOverrides.ContentType = "application/pdf";
+            return s3.GetPreSignedURL(request);
+        }
+
+        private static Stream convertExcelFiles(Stream input)
+        {
+            ExcelFileProcessor excelFileProcessor = new ExcelFileProcessor(input, "C:\\Users\\Nicholas Kan\\Documents\\Test Attachments\\uploaded\\test1.xlsx");
+            excelFileProcessor.IsSinglePDFOutput = false;
+            excelFileProcessor.WaitTimeinMilliSeconds = 5000;
+            excelFileProcessor.FailureAttemptCount = 10;
+            var (converted, message, PdfOutputFilePath, output) = excelFileProcessor.ConvertToPDF();
+            return output;
+        }
+    }
+}
