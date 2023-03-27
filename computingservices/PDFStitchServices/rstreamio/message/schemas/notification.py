015660abd5f271c4eefdbc8fe1fbf43d9e09d851
divyav-aot <divyav-aot@github>
2023-03-27 19:21:24 -0400

    notification changes to pdfstitch

--- computingservices/PDFStitchServices/rstreamio/message/schemas/notification.py
+++ computingservices/PDFStitchServices/rstreamio/message/schemas/notification.py
@@ -9,7 +9,6 @@ __author__      = "sumathi.thirumani@aot-technologies.com"
 """
 
 class NotificationPublishSchema(object):
-    batch = fields.Str(data_key="batch",allow_none=False)
     ministryrequestid = fields.Int(data_key="ministryrequestid",allow_none=False) 
     serviceid = fields.Str(data_key="serviceid",allow_none=False)
     errorflag = fields.Str(data_key="errorflag",allow_none=False)
