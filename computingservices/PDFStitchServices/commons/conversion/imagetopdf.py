5da2ded5306f29b69a91b4b719b9d80f0cc1147c
divyav-aot <divyav-aot@github>
2023-03-28 17:14:49 -0400

    changes to the imagetopdf

--- computingservices/PDFStitchServices/commons/conversion/imagetopdf.py
+++ computingservices/PDFStitchServices/commons/conversion/imagetopdf.py
@@ -8,11 +8,13 @@ from reportlab.lib.utils import ImageReader
 # Using ReportLab to insert image into PDF
 def getimagepdf(raw_image_bytes):
     imgtemp = BytesIO()
-    empty_canvas = canvas.Canvas(imgtemp)
     image = ImageReader(BytesIO(raw_image_bytes))
     iw, ih = image.getSize()
+    pagesize = (iw, ih)
     print("Image W >>> ", iw)
     print("Image H >>> ", ih)
+    empty_canvas = canvas.Canvas(imgtemp, pagesize=pagesize)
+    
     # Draw image on Canvas and save PDF in buffer
     empty_canvas.drawImage(image, 160, 160, 399, 760, preserveAspectRatio=True)    ## at (399,760) with size 160x160
     empty_canvas.save()
