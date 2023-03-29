6cfadcc2d1acdfdb99fb91eab2544fab469ac6de
Richard Qi <richard.qi@aot-technologies.com>
2023-03-29 10:08:52 -0700

    Remove comment about filter by ministry: we dont have ministry code in doc reviewer so cannot filter by ministry

--- api/reviewer_api/services/documentservice.py
+++ api/reviewer_api/services/documentservice.py
@@ -36,7 +36,7 @@ class documentservice:
 
     def getdocuments(self, requestid):
         documents = Document.getdocuments(requestid)
-        divisions = ProgramAreaDivision.getallprogramareadivisons() # TODO: What cant we filter by ministry?
+        divisions = ProgramAreaDivision.getallprogramareadivisons()
 
         formated_documents = []
         for document in documents:
