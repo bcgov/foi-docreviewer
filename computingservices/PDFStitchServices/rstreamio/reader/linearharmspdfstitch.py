9f0cf26a838f35c4c5b1d91d179ccfe7891abb8d
sumathi.thirumani <sumathi.thirumani@aot-technologies.com>
2023-04-25 17:45:16 -0700

    Changes to have separate redis for notification.

--- computingservices/PDFStitchServices/rstreamio/reader/linearharmspdfstitch.py
+++ computingservices/PDFStitchServices/rstreamio/reader/linearharmspdfstitch.py
@@ -30,35 +30,38 @@ class StartFrom(str, Enum):
 
 @app.command()
 def start(consumer_id: str, start_from: StartFrom = StartFrom.latest):
-    rdb = redisstreamdb
-    stream = rdb.Stream(STREAM_KEY)
-    last_id = rdb.get(LAST_ID_KEY.format(consumer_id=consumer_id))
-    if last_id:
-        logging.info(f"Resume from ID: {last_id}")
-    else:
-        last_id = start_from.value
-        logging.info(f"Starting from {start_from.name}")
-
-    while True:
-        logging.info("Reading stream...")
-        messages = stream.read(last_id=last_id, block=BLOCK_TIME)
-        if messages:
-            for _messages in messages:          
-                # message_id is the random id created to identify the message
-                # message is the actual data passed to the stream 
-                message_id, message = _messages 
-                logging.info(f"processing {message_id}::{message}")
-                print(f"processing {message_id}::{message}")
-                handlemessage(message)
-                # simulate processing
-                # time.sleep(random.randint(1, 3)) #TODO : todo: remove!
-                last_id = message_id
-                rdb.set(LAST_ID_KEY.format(consumer_id=consumer_id), last_id)
-                logging.info(f"finished processing {message_id}")
-                print(f"finished processing {message_id}")
-                stream.delete(message_id)                
+    try:
+        rdb = redisstreamdb
+        stream = rdb.Stream(STREAM_KEY)
+        last_id = rdb.get(LAST_ID_KEY.format(consumer_id=consumer_id))
+        if last_id:
+            logging.info(f"Resume from ID: {last_id}")
         else:
-            logging.info(f"No new messages after ID: {last_id}")
+            last_id = start_from.value
+            logging.info(f"Starting from {start_from.name}")
+
+        while True:
+            logging.info("Reading stream...")
+            messages = stream.read(last_id=last_id, block=BLOCK_TIME)
+            if messages:
+                for _messages in messages:          
+                    # message_id is the random id created to identify the message
+                    # message is the actual data passed to the stream 
+                    message_id, message = _messages 
+                    logging.info(f"processing {message_id}::{message}")
+                    print(f"processing {message_id}::{message}")
+                    handlemessage(message)
+                    # simulate processing
+                    # time.sleep(random.randint(1, 3)) #TODO : todo: remove!
+                    last_id = message_id
+                    rdb.set(LAST_ID_KEY.format(consumer_id=consumer_id), last_id)
+                    logging.info(f"finished processing {message_id}")
+                    print(f"finished processing {message_id}")
+                    stream.delete(message_id)                
+            else:
+                logging.info(f"No new messages after ID: {last_id}")
+    except(Exception) as error:
+            print(error)
 
 def handlemessage(message):
 
@@ -66,9 +69,7 @@ def handlemessage(message):
         _message = json.dumps({key.decode('utf-8'): value.decode('utf-8') for key, value in message.items()})
         _message = _message.replace("b'","'").replace("'",'')
         try:
-
             producermessage = get_in_divisionpdfmsg(_message)            
-            pdfstitchservice().processmessage(producermessage)
-               
+            pdfstitchservice().processmessage(producermessage)               
         except(Exception) as error:
-            logging.exception(error)
+            print(error)
