f2f9a74a37ad84a2d923aa25e5b985b56a56525f
unknown <nicholas.kan@aot-technologies.com>
2023-05-12 09:50:46 -0700

    disable redact button for individual redlines

--- web/src/components/FOI/Home/Redlining.js
+++ web/src/components/FOI/Home/Redlining.js
@@ -77,7 +77,7 @@ const Redlining = React.forwardRef(({
         initialDoc: currentDocumentS3Url,
         fullAPI: true,
         enableRedaction: true,
-        disabledElements: ['modalRedactButton']
+        disabledElements: ['modalRedactButton', 'annotationRedactButton']
       },
       viewer.current,
     ).then((instance) => {
