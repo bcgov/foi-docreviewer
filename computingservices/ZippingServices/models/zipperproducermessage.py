2602557622e06f79746419123298aac0c884478a
Abin Antony <abin.antony@aot-technologies.com>
2023-06-27 10:45:42 -0700

    #3908 - Zipper produce message parser

--- computingservices/ZippingServices/models/zipperproducermessage.py
+++ computingservices/ZippingServices/models/zipperproducermessage.py
@@ -1,10 +1,9 @@
 class zipperproducermessage(object):
-    def __init__(self,filesbydivision,bcgovcode,requestnumber,ministryrequestid,attributes,trigger,createdby) -> None:
-        self.filesbydivision = filesbydivision
-        self.bcgovcode=bcgovcode
-        self.requestnumber = requestnumber     
-        self.ministryrequestid=ministryrequestid
-        self.attributes=attributes             
-        self.trigger=trigger
-        self.createdby=createdby
-      
+    def __init__(self,requestid,category,requestnumber,bcgovcode,createdby,ministryrequestid,filestozip) -> None:
+        self.requestid = requestid
+        self.category=category
+        self.requestnumber = requestnumber
+        self.bcgovcode = bcgovcode
+        self.createdby = createdby
+        self.ministryrequestid = ministryrequestid
+        self.filestozip = filestozip
---
+++ computingservices/ZippingServices/utils/jsonmessageparser.py
@@ -0,0 +1,12 @@
+import json
+from models import zipperproducermessage, s3credentials
+
+def getdedupeproducermessage(producer_json):
+    j = json.loads(producer_json)
+    messageobject = zipperproducermessage(**j)
+    return messageobject
+
+def gets3credentialsobject(s3cred_json):
+    j = json.loads(s3cred_json)
+    messageobject = s3credentials(**j)
+    return messageobject    
