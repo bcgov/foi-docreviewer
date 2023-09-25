371341ed85ca90e01eeb036f5c34ccb573336df0
Abin Antony <abin.antony@aot-technologies.com>
2023-09-25 13:20:47 -0700

    #4510 redis Stream msg del and err hndlg -zipper

--- computingservices/ZippingServices/services/foirediszipperconsumer.py
+++ computingservices/ZippingServices/services/foirediszipperconsumer.py
@@ -29,51 +29,54 @@ class StartFrom(str, Enum):
 
 @app.command()
 def start(consumer_id: str, start_from: StartFrom = StartFrom.latest):
-    rdb = redisstreamdb
-    stream = rdb.Stream(STREAM_KEY)
+    try:
+        rdb = redisstreamdb
+        stream = rdb.Stream(STREAM_KEY)
 
-    last_id = rdb.get(LAST_ID_KEY.format(consumer_id=consumer_id))
-    if last_id:
-        print(f"Resume from ID: {last_id}")
-    else:
-        last_id = start_from.value
-        print(f"Starting from {start_from.name}")
+        last_id = rdb.get(LAST_ID_KEY.format(consumer_id=consumer_id))
+        if last_id:
+            print(f"Resume from ID: {last_id}")
+        else:
+            last_id = start_from.value
+            print(f"Starting from {start_from.name}")
 
-    while True:
-        # print("Reading stream...")
-        messages = stream.read(last_id=last_id, block=BLOCK_TIME)
-        if messages:
-            for message_id, message in messages:
-                print(f"processing {message_id}::{message}")
-                if message is not None:
-                    readyfornotification = False
-                    producermessage = None
-                    try:
-                        _message = json.dumps(
-                            {
-                                key.decode("utf-8"): value.decode("utf-8")
-                                for (key, value) in message.items()
-                            }
-                        )
-                        producermessage = jsonmessageparser.getzipperproducermessage(
-                            _message
-                        )
-                        print(producermessage)
-                        processmessage(producermessage)
-                        readyfornotification = True
-                        print(
-                            "Processing is completed for Job ID {0} for category {1}".format(
-                                producermessage.jobid, producermessage.category
+        while True:
+            # print("Reading stream...")
+            messages = stream.read(last_id=last_id, block=BLOCK_TIME)
+            if messages:
+                for message_id, message in messages:
+                    print(f"processing {message_id}::{message}")
+                    if message is not None:
+                        readyfornotification = False
+                        producermessage = None
+                        try:
+                            _message = json.dumps(
+                                {
+                                    key.decode("utf-8"): value.decode("utf-8")
+                                    for (key, value) in message.items()
+                                }
                             )
-                        )
-                    except Exception as error:
-                        print(
-                            "Exception while processing redis message, func start(p1), Error : {0} ".format(
-                                error
+                            producermessage = jsonmessageparser.getzipperproducermessage(
+                                _message
                             )
-                        )
-                    sendnotification(readyfornotification, producermessage)
-                last_id = message_id
-                rdb.set(LAST_ID_KEY.format(consumer_id=consumer_id), last_id)
-        # else:
-        # print(f"No new messages after ID: {last_id}")
+                            print(producermessage)
+                            processmessage(producermessage)
+                            readyfornotification = True
+                            print(
+                                "Processing is completed for Job ID {0} for category {1}".format(
+                                    producermessage.jobid, producermessage.category
+                                )
+                            )
+                            sendnotification(readyfornotification, producermessage)
+                        except Exception as error:
+                            print(
+                                "Exception while processing redis message, func start(p1), Error : {0} ".format(
+                                    str(error)
+                                )
+                            )
+                        finally:
+                            last_id = message_id
+                            rdb.set(LAST_ID_KEY.format(consumer_id=consumer_id), last_id)
+                            stream.delete(message_id)                                        
+    except Exception as ex:
+       print("Exception happened at the top level of zipper service: ", str(ex))
