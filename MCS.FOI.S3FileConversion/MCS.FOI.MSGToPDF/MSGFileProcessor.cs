5888a49bf191f892b56fe66d8b3600a41b94e91c
unknown <nicholas.kan@aot-technologies.com>
2023-10-03 12:55:59 -0700

    debug commit 5

--- MCS.FOI.S3FileConversion/MCS.FOI.MSGToPDF/MSGFileProcessor.cs
+++ MCS.FOI.S3FileConversion/MCS.FOI.MSGToPDF/MSGFileProcessor.cs
@@ -13,6 +13,8 @@ using System.Collections.Generic;
 using Syncfusion.Pdf.HtmlToPdf;
 using System.ComponentModel.DataAnnotations;
 using System;
+using RtfPipe.Tokens;
+using System.IO;
 
 namespace MCS.FOI.MSGToPDF
 {
@@ -97,9 +99,6 @@ namespace MCS.FOI.MSGToPDF
 
                                             filename = Path.GetFileNameWithoutExtension(filename) + '1' + extension;
                                         }
-                                        Console.WriteLine("attachmentname: " + _attachment.FileName);
-                                        Console.WriteLine("attachmentpos: " + _attachment.RenderingPosition);
-                                        Console.WriteLine("attachmentmime: " + extension);
                                         fileNameHash.Add(filename, true);
                                         attachmentInfo.Add("filename", _attachment.FileName);
                                         attachmentInfo.Add("s3filename", filename);
@@ -190,17 +189,14 @@ namespace MCS.FOI.MSGToPDF
                                                 bodyreplaced = ReplaceFirstOccurrence(bodyreplaced, rtfInlineObject, " <b>[**Inline Attachment - " + inlineAttachment.FileName + "**]</b>");
                                             }
                                         }
-                                    }                                    
+                                    }
                                 }
 
-                                
                                 byte[] byteArray = Encoding.ASCII.GetBytes(bodyreplaced);
                                 using (MemoryStream messageStream = new MemoryStream(byteArray))
-                                {
-                                    //messageStream.WriteTo(fs4);
+                                {                                    
                                     using (WordDocument rtfDoc = new WordDocument(messageStream, Syncfusion.DocIO.FormatType.Html))
                                     {
-                                        //rtfDoc.Save(fs3, FormatType.Docx);
                                         // Replace leading tabs, issue with syncfusion
                                         rtfDoc.ReplaceFirst = true;
                                         var regex = new Regex(@"(\r)*(\n)*(\t)+", RegexOptions.Multiline);
@@ -208,21 +204,24 @@ namespace MCS.FOI.MSGToPDF
 
                                         List<Entity> pictures = rtfDoc.FindAllItemsByProperty(EntityType.Picture, "", "");
 
-                                        foreach (WPicture picture in pictures)
+                                        if (pictures != null)
                                         {
-                                            picture.LockAspectRatio = true;
-                                            const float maxSize = 500;
-                                            if (picture.Height > maxSize && picture.Height >= picture.Width)
+                                            foreach (WPicture picture in pictures)
                                             {
-                                                var scale = (maxSize / picture.Height) * 100;
-                                                picture.HeightScale = scale;
-                                                picture.WidthScale = scale;
-                                            }
-                                            if (picture.Width > maxSize)
-                                            {
-                                                var scale = (maxSize / picture.Width) * 100;
-                                                picture.HeightScale = scale;
-                                                picture.WidthScale = scale;
+                                                picture.LockAspectRatio = true;
+                                                const float maxSize = 500;
+                                                if (picture.Height > maxSize && picture.Height >= picture.Width)
+                                                {
+                                                    var scale = (maxSize / picture.Height) * 100;
+                                                    picture.HeightScale = scale;
+                                                    picture.WidthScale = scale;
+                                                }
+                                                if (picture.Width > maxSize)
+                                                {
+                                                    var scale = (maxSize / picture.Width) * 100;
+                                                    picture.HeightScale = scale;
+                                                    picture.WidthScale = scale;
+                                                }
                                             }
                                         }
 
