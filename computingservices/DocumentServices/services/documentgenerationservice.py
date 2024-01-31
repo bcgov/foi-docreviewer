1db5202794c124bab3adceb04dc0cfb2a82c4afe
Aparna <aparna.s@aot-technologies.com>
2024-01-31 13:09:55 -0800

    Integration with microservice

---
+++ computingservices/DocumentServices/services/documentgenerationservice.py
@@ -0,0 +1,59 @@
+#from reviewer_api.services.cdogs_api_service import cdogsapiservice
+from .cdogsapiservice import cdogsapiservice
+import json
+import logging
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+import os
+import boto3
+from botocore.exceptions import ClientError
+from botocore.config import Config
+import requests
+import mimetypes
+
+
+
+s3host = os.getenv("OSS_S3_HOST")
+s3region = os.getenv("OSS_S3_REGION")
+
+class documentgenerationservice:
+    """document generation Service class."""
+
+    # def __init__(self,documenttypename='redaction_summary'):
+    #     self.cdgos_api_service = cdogsapiservice()
+    #     self.documenttypename = documenttypename
+    #     receipt_document_type : DocumentType = DocumentType.get_document_type_by_name(self.documenttypename)
+    #     if receipt_document_type is None:
+    #         raise BusinessException(Error.DATA_NOT_FOUND)
+        
+    #     self.receipt_template : DocumentTemplate = DocumentTemplate \
+    #         .get_template_by_type(document_type_id = receipt_document_type.document_type_id)
+    #     if self.receipt_template is None:
+    #         raise BusinessException(Error.DATA_NOT_FOUND)  
+        
+
+    def generate_pdf(self, receipt_template, data, receipt_template_path='reviewer_api/templates/redline_redaction_summary.docx'):
+        access_token= cdogsapiservice()._get_access_token()
+        template_cached = False
+        print("\nreceipt_template:",receipt_template)
+        if receipt_template["cdogs_hash_code"]:
+            print('Checking if template %s is cached', receipt_template["cdogs_hash_code"])
+            template_cached = cdogsapiservice().check_template_cached(receipt_template["cdogs_hash_code"], access_token)
+            
+        if receipt_template["cdogs_hash_code"] is None or not template_cached:
+            receipt_template["cdogs_hash_code"] = cdogsapiservice().upload_template(receipt_template_path, access_token)
+            print('Uploading new template--->',receipt_template)
+            # receipt_template.flush()
+            # receipt_template.commit()
+        print('Generating redaction summary')
+        return cdogsapiservice().generate_pdf(receipt_template["cdogs_hash_code"], data,access_token)
+
+    # def upload_receipt(self, filename, filebytes, ministryrequestid, ministrycode, filenumber):
+    #     try:
+    #         logging.info("Upload summary for ministry request id"+ str(ministryrequestid))
+    #         _response =  self.__uploadbytes(filename, filebytes, ministrycode, filenumber)
+    #         logging.info("Upload status for payload"+ json.dumps(_response))
+    #         if _response["success"] == True:
+    #             _documentschema = {"documents": [{"filename": _response["filename"], "documentpath": _response["documentpath"]}]}
+    #         return _response
+    #     except Exception as ex:
+    #         logging.exception(ex)
--- computingservices/DocumentServices/services/redactionsummaryservice.py
+++ computingservices/DocumentServices/services/redactionsummaryservice.py
@@ -2,12 +2,68 @@
 import traceback
 import json
 from services.dal.pdfstitchjobactivity import pdfstitchjobactivity
+from .documentgenerationservice import documentgenerationservice
+from .s3documentservice import uploadbytes
+
 class redactionsummaryservice():
 
     def processmessage(self,message):
         try:
+            print("\n\nBEFORE CALL TO pdfstitchjobactivity!!")
             pdfstitchjobactivity().recordjobstatus(message,3,"redactionsummarystarted")
-            print(message)
+            print("\n\nmessage:",message)
+            #Get Data
+            #Get Template           
+            receipt_template={
+                "extension":"docx",
+                "cdogs_hash_code":"58G94G"
+            }
+            # pdfgenerationdata={
+            #     "requestid":"ABC-123-545",
+            #     "data":[{
+            #     "flagname":"DISCLOSED IN FULL",
+            #     "pagecount": 20,
+            #     "sections": [{"range":"1 - 2", "section":"disclosed in full"},{"range":"3",  "section":"disclosed in full"}, {"range":"5 - 19", "section":"disclosed in full"}]
+            #     },
+            #     {
+            #     "flagname":"DISCLOSED IN PART",
+            #     "pagecount": 5,
+            #     "sections": [{"range":"4",  "section":"partial disclosure under S.1"},
+            #                     {"range":"13 - 16", "section":"partial disclosure under S.4"}]
+            #     },
+            #     {
+            #     "flagname":"WITHHELD IN FULL",
+            #     "pagecount": 2,
+            #     "sections": [{"range":"17 - 18",  "section":"withheld in full under S.18"}]
+            #     }
+            #     ]}
+            pdfgenerationdata={'requestnumber': 'EDU-20240125-1',
+                               'data': [{'flagname': 'PARTIAL DISCLOSURE', 'pagecount': 4, 
+                                'sections': [{'range': 1, 'section': 'Partial Disclosure under s. 15'}, {'range': '5-7','section': 'Partial Disclosure under s. 15,s. 16,s. 13,s. 17,s. 12'}]}, {'flagname': 'WITHHELD IN FULL', 'pagecount': 1, 
+                                'sections': [{'range': 3, 'section': 'Withheld in Full under s. 15,s. 13,s. 12'}]}, {'flagname': 'DUPLICATE', 'pagecount': 2, 
+                                'sections': [{'range': 2, 'section': 'Duplicate'}, {'range': 4, 'section': 'Duplicate'}]}]}
+            print("\nBefore calling generate_pdf:", json.dumps(message.attributes) )
+            receipt_template_path= r'templates/redline_redaction_summary.docx'
+            redline_redaction_summary= documentgenerationservice().generate_pdf(receipt_template,pdfgenerationdata,receipt_template_path)
+            print("Finished generate_pdf!!")
+            #Method for calling the pdf generation method ends
+            #Document
+            #Upload to S3 starts
+            messagejson=json.loads(message.attributes)
+            s3uri = messagejson[0]['files'][0]['s3uripath']
+            # Find the last occurrence of '/'
+            last_slash_index = s3uri.rfind('/')
+            # Remove the filename and everything after it
+            s3uri = s3uri[:last_slash_index + 1]
+            divisionname = messagejson[0]['divisionname']
+            category = "redline" #get it from message
+            requestnumber=pdfgenerationdata["requestnumber"]
+            filename = f"{requestnumber} - {category} - {divisionname} - summary"
+            #filename = "Summary.pdf"
+            print("\nBefore calling uploadbytes", s3uri )
+            uploadbytes(filename,redline_redaction_summary.content, s3uri)
+            #Upload to S3 ends
+            #Invoke ZIP
         except (Exception) as error:
             print('error occured in redaction summary service: ', error)
     
