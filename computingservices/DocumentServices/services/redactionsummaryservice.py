bd5045debbd2f04fa8f4995323fe90401c2ea680
sumathi.thirumani <sumathi.thirumani@aot-technologies.com>
2024-02-06 09:09:44 -0800

    Changes to stitch division wise.

--- computingservices/DocumentServices/services/redactionsummaryservice.py
+++ computingservices/DocumentServices/services/redactionsummaryservice.py
@@ -9,16 +9,22 @@ class redactionsummaryservice():
         try:
             pdfstitchjobactivity().recordjobstatus(message,3,"redactionsummarystarted")
             #Get Data ; Begin
-            formattedsummary = redactionsummary().prepareredactionsummary(message)
+            divisiondocuments = json.loads(message.summarydocuments)
+            for entry in divisiondocuments:
+                divisionid = entry['divisionid']
+                documentids = entry['documentids']
+                formattedsummary = redactionsummary().prepareredactionsummary(message, documentids)
             #Get Data : End
             #Get Template
             #Document
             #Upload to S3
             #Invoke ZIP
-            print(message)
-            print(formattedsummary)
+            
+            #print(message)
+                print(formattedsummary)
         except (Exception) as error:
             print('error occured in redaction summary service: ', error)
-    
+
+   
     
 
