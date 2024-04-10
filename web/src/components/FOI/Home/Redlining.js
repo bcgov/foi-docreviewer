d9674bb2bda5aa7a390c4dcc6e8790ea1dd83514
Aparna <aparna.s@aot-technologies.com>
2024-04-10 13:48:00 -0700

    Revert incomplete update for merge

--- web/src/components/FOI/Home/Redlining.js
+++ web/src/components/FOI/Home/Redlining.js
@@ -74,7 +74,6 @@ import {
 } from "./utils";
 import { Edit, MultiSelectEdit } from "./Edit";
 import _ from "lodash";
-import FOIPPASectionsModal from "./FOIPPASectionsModal";
 
 const Redlining = React.forwardRef(
   (
@@ -2331,51 +2330,51 @@ const Redlining = React.forwardRef(
       );
     };
 
-    // const AntSwitch = styled(Switch)(({ theme }) => ({
-    //   width: 28,
-    //   height: 16,
-    //   padding: 0,
-    //   display: "flex",
-    //   "&:active": {
-    //     "& .MuiSwitch-thumb": {
-    //       width: 15,
-    //     },
-    //     "& .MuiSwitch-switchBase.Mui-checked": {
-    //       transform: "translateX(9px)",
-    //     },
-    //   },
-    //   "& .MuiSwitch-switchBase": {
-    //     padding: 2,
-    //     "&.Mui-checked": {
-    //       transform: "translateX(12px)",
-    //       color: "#fff",
-    //       "& + .MuiSwitch-track": {
-    //         opacity: 1,
-    //         backgroundColor:
-    //           theme.palette.mode === "dark" ? "#177ddc" : "#38598a",
-    //       },
-    //     },
-    //   },
-    //   "& .MuiSwitch-thumb": {
-    //     boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
-    //     width: 12,
-    //     height: 12,
-    //     borderRadius: 6,
-    //     transition: theme.transitions.create(["width"], {
-    //       duration: 200,
-    //     }),
-    //   },
-    //   "& .MuiSwitch-track": {
-    //     borderRadius: 16 / 2,
-    //     opacity: 1,
-    //     backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#38598a",
-    //     boxSizing: "border-box",
-    //   },
-    // }));
+    const AntSwitch = styled(Switch)(({ theme }) => ({
+      width: 28,
+      height: 16,
+      padding: 0,
+      display: "flex",
+      "&:active": {
+        "& .MuiSwitch-thumb": {
+          width: 15,
+        },
+        "& .MuiSwitch-switchBase.Mui-checked": {
+          transform: "translateX(9px)",
+        },
+      },
+      "& .MuiSwitch-switchBase": {
+        padding: 2,
+        "&.Mui-checked": {
+          transform: "translateX(12px)",
+          color: "#fff",
+          "& + .MuiSwitch-track": {
+            opacity: 1,
+            backgroundColor:
+              theme.palette.mode === "dark" ? "#177ddc" : "#38598a",
+          },
+        },
+      },
+      "& .MuiSwitch-thumb": {
+        boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+        width: 12,
+        height: 12,
+        borderRadius: 6,
+        transition: theme.transitions.create(["width"], {
+          duration: 200,
+        }),
+      },
+      "& .MuiSwitch-track": {
+        borderRadius: 16 / 2,
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#38598a",
+        boxSizing: "border-box",
+      },
+    }));
 
-    // const changeModalSort = (e) => {
-    //   setModalSortNumbered(e.target.checked);
-    // };
+    const changeModalSort = (e) => {
+      setModalSortNumbered(e.target.checked);
+    };
 
     const changeSortOrder = (e) => {
       if (modalSortNumbered) {
@@ -4032,17 +4031,17 @@ const Redlining = React.forwardRef(
       return {"sorteddocuments": sorteddocids, "pkgdocuments": summarylist}   
     }
 
-    // const compareValues = (a, b) => {
-    //   if (modalSortNumbered) {
-    //     if (modalSortAsc) {
-    //       return a.id - b.id;
-    //     } else {
-    //       return b.id - a.id;
-    //     }
-    //   } else {
-    //     return b.count - a.count;
-    //   }
-    // };
+    const compareValues = (a, b) => {
+      if (modalSortNumbered) {
+        if (modalSortAsc) {
+          return a.id - b.id;
+        } else {
+          return b.id - a.id;
+        }
+      } else {
+        return b.count - a.count;
+      }
+    };
     const decodeAstr = (astr) => {
       const parser = new DOMParser()
       const xmlDoc = parser.parseFromString(astr, "text/xml")
@@ -4099,7 +4098,120 @@ const Redlining = React.forwardRef(
     return (
       <div>
         <div className="webviewer" ref={viewer}></div>
-        <FOIPPASectionsModal />
+        <ReactModal
+          initWidth={650}
+          initHeight={700}
+          minWidth={400}
+          minHeight={200}
+          className={"state-change-dialog"}
+          onRequestClose={cancelRedaction}
+          isOpen={modalOpen}
+        >
+          <DialogTitle disableTypography id="state-change-dialog-title">
+            <h2 className="state-change-header">FOIPPA Sections</h2>
+            <IconButton className="title-col3" onClick={cancelRedaction}>
+              <i className="dialog-close-button">Close</i>
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent className={"dialog-content-nomargin"}>
+            <DialogContentText
+              id="state-change-dialog-description"
+              component={"span"}
+            >
+              <Stack direction="row-reverse" spacing={1} alignItems="center">
+                <button
+                  onClick={changeSortOrder}
+                  style={{
+                    border: "none",
+                    backgroundColor: "white",
+                    padding: 0,
+                  }}
+                  disabled={!modalSortNumbered}
+                >
+                  {modalSortAsc ? (
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      size="1x"
+                      color="#666666"
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faArrowDown}
+                      size="1x"
+                      color="#666666"
+                    />
+                  )}
+                </button>
+                <Typography>Numbered Order</Typography>
+                <AntSwitch
+                  onChange={changeModalSort}
+                  checked={modalSortNumbered}
+                  inputProps={{ "aria-label": "ant design" }}
+                />
+                <Typography>Most Used</Typography>
+              </Stack>
+              <div style={{ overflowY: "scroll" }}>
+                <List className="section-list">
+                  {sections?.sort(compareValues).map((section, index) => (
+                    <ListItem key={"list-item" + section.id}>
+                      <input
+                        type="checkbox"
+                        className="section-checkbox"
+                        key={"section-checkbox" + section.id}
+                        id={"section" + section.id}
+                        data-sectionid={section.id}
+                        onChange={handleSectionSelected}
+                        disabled={sectionIsDisabled(section.id)}
+                        defaultChecked={selectedSections.includes(section.id)}
+                      />
+                      <label
+                        key={"list-label" + section.id}
+                        className="check-item"
+                      >
+                        {section.section + " - " + section.description}
+                      </label>
+                    </ListItem>
+                  ))}
+                </List>
+              </div>
+              {/* <span className="confirmation-message">
+                  Are you sure you want to delete the attachments from this request? <br></br>
+                  <i>This will remove all attachments from the redaction app.</i>
+                </span> */}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions className="foippa-modal-actions">
+            <button
+              className={`btn-bottom btn-save btn`}
+              onClick={editRedacts ? saveRedactions : saveRedaction}
+              disabled={saveDisabled}
+            >
+              Select Code(s)
+            </button>
+            {defaultSections.length > 0 ? (
+              <button
+                className="btn-bottom btn-cancel"
+                onClick={clearDefaultSections}
+              >
+                Clear Defaults
+              </button>
+            ) : (
+              <button
+                className={`btn-bottom btn-cancel ${
+                  saveDisabled && "btn-disabled"
+                }`}
+                onClick={saveDefaultSections}
+                disabled={saveDisabled}
+              >
+                Save as Default
+              </button>
+            )}
+            <button className="btn-bottom btn-cancel" onClick={cancelRedaction}>
+              Cancel
+            </button>
+          </DialogActions>
+        </ReactModal>
         <ReactModal
           initWidth={800}
           initHeight={300}
