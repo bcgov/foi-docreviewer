6f122ddd2a81d9ecb5c78827cb61bf37e893f3b3
Milos Despotovic <milos.despotovic@gov.bc.ca>
2024-05-09 13:41:26 -0700

    Update presigned redline endpoint to handle consult

--- api/reviewer_api/resources/foiflowmasterdata.py
+++ api/reviewer_api/resources/foiflowmasterdata.py
@@ -192,6 +192,8 @@ class FOIFlowS3PresignedRedline(Resource):
             packagetype = "redline"
             if redactionlayer == "oipc":
                 packagetype = "oipcreview" if layertype == "oipcreview" else "oipcredline"
+            if layertype == "consult":
+                packagetype = "consult"
 
             for div in data["divdocumentList"]:
                 if len(div["documentlist"]) > 0:
@@ -202,6 +204,10 @@ class FOIFlowS3PresignedRedline(Resource):
                         filepath_put = "{0}/{2}/{1}/{0} - {2} - {1}.pdf".format(
                             filepathlist[0], division_name, packagetype
                         )
+                        if packagetype == "consult":
+                            filepath_put = "{0}/{2}/{2} - {1} - {0}.pdf".format(
+                                filepathlist[0], division_name, packagetype
+                            )
 
                         s3path_save = s3client.generate_presigned_url(
                             ClientMethod="get_object",
