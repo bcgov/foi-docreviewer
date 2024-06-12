4b165bce3838eff95b8a2cad59330e9bbce09fb4
Aparna <aparna.s@aot-technologies.com>
2024-06-12 14:08:44 -0700

    Missing field added in summary

--- computingservices/DocumentServices/rstreamio/message/schemas/redactionsummary.py
+++ computingservices/DocumentServices/rstreamio/message/schemas/redactionsummary.py
@@ -30,7 +30,7 @@ class Summary(Schema):
 
 class RedactionSummaryMessage(object):
     def __init__(self, jobid, requestid, ministryrequestid, category, requestnumber, 
-                 bcgovcode, createdby, filestozip, finaloutput, attributes, summarydocuments ,redactionlayerid,requesttype) -> None:
+                 bcgovcode, createdby, filestozip, finaloutput, attributes, summarydocuments ,redactionlayerid, requesttype, feeoverridereason) -> None:
         self.jobid = jobid
         self.requestid = requestid
         self.ministryrequestid = ministryrequestid
@@ -44,6 +44,7 @@ class RedactionSummaryMessage(object):
         self.summarydocuments = summarydocuments
         self.redactionlayerid = redactionlayerid
         self.requesttype = requesttype
+        self.feeoverridereason = feeoverridereason
 
 
 def get_in_redactionsummary_msg(producer_json): 
