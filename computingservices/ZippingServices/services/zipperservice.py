782074ed2ec9a464744074d507c6a168a82c903a
Abin Antony <abin.antony@aot-technologies.com>
2024-09-18 17:53:43 -0700

    #FOIMOD-3469 removed condition for metadata!

--- computingservices/ZippingServices/services/zipperservice.py
+++ computingservices/ZippingServices/services/zipperservice.py
@@ -117,7 +117,10 @@ def __zipfilesandupload(_message, s3credentials):
                     
                     try:                           
                         _formattedbytes = __removesensitivecontent(_docbytes)
-                        print("_formattedbytes length is {0}".format(len(_formattedbytes)))                           
+                        if _formattedbytes is not None:
+                            print("_formattedbytes length is {0}".format(len(_formattedbytes)))
+                        else:
+                           print("_formattedbytes is none")                                
                     except Exception:
                         print("error happened while removing sensitive content of {0} ".format(filename))
                         print(traceback.format_exc())
@@ -152,17 +155,17 @@ def __removesensitivecontent(documentbytes):
     # clear metadata
     reader2 = PyPDF2.PdfReader(BytesIO(documentbytes))
     # Check if metadata exists.
-    if reader2.metadata is not None:
-        # Create a new PDF file without metadata.
-        writer = PyPDF2.PdfWriter()
-        # Copy pages from the original PDF to the new PDF.
-        for page_num in range(len(reader2.pages)):
-            page = reader2.pages[page_num]                
-            writer.add_page(page)        
-        #writer.remove_links() # to remove comments.
-        buffer = BytesIO()
-        writer.write(buffer)
-        return buffer.getvalue()
+    #if reader2.metadata is not None:
+    # Create a new PDF file without metadata.
+    writer = PyPDF2.PdfWriter()
+    # Copy pages from the original PDF to the new PDF.
+    for page_num in range(len(reader2.pages)):
+        page = reader2.pages[page_num]                
+        writer.add_page(page)        
+    #writer.remove_links() # to remove comments.
+    buffer = BytesIO()
+    writer.write(buffer)
+    return buffer.getvalue()
 
 
 def __getzipfilepath(foldername, filename):
