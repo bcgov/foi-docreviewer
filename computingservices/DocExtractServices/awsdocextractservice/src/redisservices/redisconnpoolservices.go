3e8a36fd29ec9fcaeac390745a87e4b28bae2be3
Abin Antony <abin.antony@aot-technologies.com>
2024-10-08 16:36:40 -0700

    #FOIMOD-3454  sample ebv and udpate to redis pool service

--- computingservices/DocExtractServices/awsdocextractservice/src/redisservices/redisconnpoolservices.go
+++ computingservices/DocExtractServices/awsdocextractservice/src/redisservices/redisconnpoolservices.go
@@ -1,7 +1,10 @@
 package redisservices
 
 import (
+	"log"
+
 	"github.com/gomodule/redigo/redis"
+	"github.com/spf13/viper"
 )
 
 func GetFOIRedisConnectionPool() *redis.Pool {
@@ -11,9 +14,41 @@ func GetFOIRedisConnectionPool() *redis.Pool {
 		MaxIdle:   5,
 		Wait:      true,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "localhost:6379")
+			//return redis.Dial("tcp", "localhost:6379")
+			return redis.Dial("tcp", viperEnvVariable("redishostip")+":"+viperEnvVariable("redisport"), redis.DialPassword(viperEnvVariable("redispassword")))
 		},
 	}
 
 	return redisPool
+}
+
+// use viper package to read .env file
+// return the value of the key
+func viperEnvVariable(key string) string {
+
+	// SetConfigFile explicitly defines the path, name and extension of the config file.
+	// Viper will use this and not check any of the config paths.
+	// .env - It will search for the .env file in the current directory
+	viper.SetConfigFile("./.env")
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
 }
