2b8a75d1e1043f0c028192dd60073a5d8253fe49
Aparna <aparna.s@aot-technologies.com>
2024-10-16 08:51:42 -0700

    FOIMOD-3073-Fees - Remaining Balance Check
    Harms download & Redline notification fixes

--- computingservices/ZippingServices/models/redlineresponsenotificationmessage.py
+++ computingservices/ZippingServices/models/redlineresponsenotificationmessage.py
@@ -1,5 +1,5 @@
 class redlineresponsenotificationmessage(object):
-    def __init__(self, ministryrequestid, serviceid, errorflag, createdby,feeoverridereason) -> None:
+    def __init__(self, ministryrequestid, serviceid, errorflag, createdby,feeoverridereason="") -> None:
         self.ministryrequestid = ministryrequestid
         self.serviceid = serviceid
         self.errorflag = errorflag
