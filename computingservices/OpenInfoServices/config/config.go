77d3c19343b12cabfee2c70d8bddfbe9c1bf7076
Richard Qi <richard.qi@aot-technologies.com>
2024-12-18 21:53:21 -0800

    move code to dev-rook branch instead of test-rook/test-marshal

---
+++ computingservices/OpenInfoServices/config/config.go
@@ -0,0 +1,155 @@
+package myconfig
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	//DB
+	host     string
+	port     string
+	user     string
+	password string
+	dbname   string
+
+	//Redis
+	queue         string
+	queuehost     string
+	queueport     string
+	queuepassword string
+
+	//S3
+	s3url         string
+	oibucket      string
+	oiprefix      string
+	sitemapprefix string
+	sitemaplimit  int
+	region        string
+	accessKey     string
+	secretKey     string
+	s3host        string
+
+	env string
+
+	onceDB     sync.Once
+	onceRedis  sync.Once
+	onceS3     sync.Once
+	onceS3Path sync.Once
+	onceOthers sync.Once
+)
+
+// use viper package to read .env file
+// return the value of the key
+func viperEnvVariable(key string) string {
+
+	// SetConfigFile explicitly defines the path, name and extension of the config file.
+	// Viper will use this and not check any of the config paths.
+	// .env - It will search for the .env file in given path
+	viper.SetConfigFile(getEnv("ENVFILE_PATH"))
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(key).(string)
+
+	// If the type is a string then ok will be true
+	// ok will make sure the program not break
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
+}
+
+// Lazy initialization functions
+func loadConfigDB() {
+	host = getEnv("FOI_DB_HOST")
+	port = getEnv("FOI_DB_PORT")
+	user = getEnv("FOI_DB_USER")
+	password = getEnv("FOI_DB_PASSWORD")
+	dbname = getEnv("FOI_DB_NAME")
+}
+
+func loadConfigRedis() {
+	queuehost = getEnv("OI_REDIS_HOST")
+	queueport = getEnv("OI_REDIS_PORT")
+	queuepassword = getEnv("OI_REDIS_PASSWORD")
+}
+
+func loadConfigS3() {
+	region = getEnv("OI_S3_REGION")
+	accessKey = getEnv("OI_ACCESS_KEY")
+	secretKey = getEnv("OI_SECRET_KEY")
+	s3host = getEnv("OI_S3_HOST")
+}
+
+func loadConfigS3Path() {
+	s3url = "https://" + getEnv("OI_S3_HOST") + "/"
+	oibucket = getEnv("OI_S3_BUCKET")
+	oiprefix = getEnv("OI_PREFIX")
+	sitemapprefix = getEnv("SITEMAP_PREFIX")
+
+	var strerr error
+	sitemaplimit, strerr = strconv.Atoi(getEnv("SITEMAP_PAGES_LIMIT"))
+	if strerr != nil {
+		log.Printf("Error converting string to int for SITEMAP_PAGES_LIMIT, will use default value: %v", strerr)
+		sitemaplimit = 5000
+	}
+}
+
+func loadConfigOther() {
+	env = getEnv("OI_S3_ENV")
+	queue = getEnv("OI_QUEUE_NAME")
+}
+
+// Helper function to get environment variables
+func getEnv(key string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return viperEnvVariable(key)
+	}
+	return value
+}
+
+// GetDB retrieves the database variables with lazy initialization
+func GetDB() (string, string, string, string, string) {
+	onceDB.Do(loadConfigDB) // Ensures loadConfig is called only once
+	return host, port, user, password, dbname
+}
+
+// GetRedis retrieves the redis variables with lazy initialization
+func GetRedis() (string, string, string) {
+	onceRedis.Do(loadConfigRedis) // Ensures loadConfig is called only once
+	return queuehost, queueport, queuepassword
+}
+
+// GetS3 retrieves the S3 variables with lazy initialization
+func GetS3() (string, string, string, string) {
+	onceS3.Do(loadConfigS3) // Ensures loadConfig is called only once
+	return region, accessKey, secretKey, s3host
+}
+
+// GetS3 retrieves the S3 variables with lazy initialization
+func GetS3Path() (string, string, string, string, int) {
+	onceS3Path.Do(loadConfigS3Path) // Ensures loadConfig is called only once
+	return s3url, oibucket, oiprefix, sitemapprefix, sitemaplimit
+}
+
+// GetS3 retrieves the S3 variables with lazy initialization
+func GetOthers() (string, string) {
+	onceOthers.Do(loadConfigOther) // Ensures loadConfig is called only once
+	return env, queue
+}
---
+++ computingservices/OpenInfoServices/lib/db/dbservices.go
@@ -0,0 +1,417 @@
+package dbservice
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dateformat              = "2006-01-02"
+	openstate_ready         = "Ready to Publish"
+	openstate_published     = "Published"
+	openstate_unpublish     = "Unpublish"
+	oirequesttype_publish   = "Publish"
+	oirequesttype_unpublish = "Unpublish Request"
+	openstatus_ready        = "ready"
+	openstatus_unpublish    = "unpublished"
+)
+
+type AdditionalFile struct {
+	Additionalfileid int
+	Filename         string
+	S3uripath        string
+}
+
+type OpenInfoRecord struct {
+	Openinfoid           int
+	Foiministryrequestid int
+	Axisrequestid        string
+	Description          string
+	Published_date       string
+	Contributor          string
+	Applicant_type       string
+	Fees                 float64
+	BCgovcode            string
+	Sitemap_pages        string
+	Type                 string
+	Additionalfiles      []AdditionalFile
+}
+
+func Conn(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	// fmt.Println("Successfully connected!")
+	return db, nil
+}
+
+func UpdateOIRecordStatus(db *sql.DB, foiministryrequestid int, publishingstatus string, message string) error {
+
+	// Begin a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Error beginning transaction: %v", err)
+	}
+
+	// Step 1: Set previous versions' isactive to false
+	_, err = tx.Exec(`UPDATE public."FOIOpenInformationRequests" SET isactive = false, updated_at = $2, updatedby = 'publishingservice' WHERE foiministryrequest_id = $1 AND isactive = true`, foiministryrequestid, time.Now())
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error updating previous versions: %v", err)
+	}
+
+	// Step 2: Insert a new version of the record
+	_, err = tx.Exec(`
+        INSERT INTO public."FOIOpenInformationRequests" (version, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, isactive, copyrightsevered, created_at, updated_at, createdby, updatedby, processingstatus, processingmessage, sitemap_pages)
+        SELECT version + 1, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, true, copyrightsevered, $2, NULL, 'publishingservice', 'publishingservice', $3, $4, sitemap_pages
+        FROM public."FOIOpenInformationRequests"
+        WHERE foiministryrequest_id = $1 AND isactive = false
+        ORDER BY version DESC
+        LIMIT 1
+    `, foiministryrequestid, time.Now(), publishingstatus, message)
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error inserting new version for status: %v", err)
+	}
+
+	// Commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("Error committing transaction: %v", err)
+	}
+
+	return err
+}
+
+func GetOIRecordsForPrePublishing(db *sql.DB) ([]OpenInfoRecord, error) {
+	var records []OpenInfoRecord
+	// var record OpenInfoRecord
+
+	// Get the current time
+	now := time.Now()
+	// Add 24 hours to the current time
+	tomorrow := now.Add(24 * time.Hour)
+
+	qry := fmt.Sprintf(`
+		SELECT
+			oi.foiopeninforequestid,
+			mr.foiministryrequestid,
+			mr.axisrequestid,
+			mr.description,
+			oi.publicationdate,
+			pa.name as contributor,
+			ac.name as applicant_type,
+			COALESCE((fee.feedata->>'amountpaid')::Numeric, 0) as fees,
+			LOWER(pa.bcgovcode),
+			COALESCE(oi.sitemap_pages, '') as sitemap_pages,
+			'publish' as type,
+			oifiles.additionalfileid,
+			oifiles.filename,
+			oifiles.s3uripath
+		FROM public."FOIMinistryRequests" mr
+		INNER JOIN public."FOIRequests" r on mr.foirequest_id = r.foirequestid and mr.foirequestversion_id = r.version
+		INNER JOIN public."ProgramAreas" pa on mr.programareaid = pa.programareaid
+		INNER JOIN public."ApplicantCategories" ac on r.applicantcategoryid = ac.applicantcategoryid
+		LEFT JOIN (
+			SELECT ministryrequestid, MAX(version) as max_version
+			FROM public."FOIRequestCFRFees"
+			GROUP BY ministryrequestid
+		) latest_payment on mr.foiministryrequestid = latest_payment.ministryrequestid
+		LEFT JOIN public."FOIRequestCFRFees" fee on mr.foiministryrequestid = fee.ministryrequestid 
+			and latest_payment.max_version = fee.version and mr.version = fee.ministryrequestversion
+		INNER JOIN public."FOIOpenInformationRequests" oi on mr.foiministryrequestid = oi.foiministryrequest_id and oi.isactive = TRUE
+		INNER JOIN public."OpenInformationStatuses" oistatus on mr.oistatus_id = oistatus.oistatusid
+		INNER JOIN public."OpenInfoPublicationStatuses" oirequesttype on oi.oipublicationstatus_id = oirequesttype.oipublicationstatusid
+		LEFT JOIN public."FOIOpenInfoAdditionalFiles" oifiles on mr.foiministryrequestid = oifiles.ministryrequestid
+		WHERE (oistatus.name = '%s' or oistatus.name = '%s') and oirequesttype.name = '%s' and oi.publicationdate < '%s' and oi.processingstatus is NULL and mr.isactive = TRUE
+	`, openstate_ready, openstate_published, oirequesttype_publish, tomorrow.Format(dateformat))
+
+	rows, err := db.Query(qry)
+	if err != nil {
+		return records, fmt.Errorf("query failed: %w", err)
+	}
+	defer rows.Close()
+
+	oiRecordsMap := make(map[int]*OpenInfoRecord)
+	for rows.Next() {
+		var openinfoid, foiministryrequestid, additionalfileid sql.NullInt64
+		var axisrequestid, description, published_date, contributor, applicant_type, bcgovcode, sitemap_pages, queuetype, filename, s3uripath sql.NullString
+		var fees sql.NullFloat64
+
+		// err := rows.Scan(
+		// 	&record.Openinfoid,
+		// 	&record.Foiministryrequestid,
+		// 	&record.Axisrequestid,
+		// 	&record.Description,
+		// 	&record.Published_date,
+		// 	&record.Contributor,
+		// 	&record.Applicant_type,
+		// 	&record.Fees,
+		// 	&record.BCgovcode,
+		// 	&record.Sitemap_pages,
+		// 	&record.Type,
+		// )
+		err := rows.Scan(
+			&openinfoid,
+			&foiministryrequestid,
+			&axisrequestid,
+			&description,
+			&published_date,
+			&contributor,
+			&applicant_type,
+			&fees,
+			&bcgovcode,
+			&sitemap_pages,
+			&queuetype,
+			&additionalfileid,
+			&filename,
+			&s3uripath,
+		)
+		if err != nil {
+			return records, fmt.Errorf("failed to retrieve query result for prepublish: %w", err)
+		}
+
+		if openinfoid.Valid && foiministryrequestid.Valid && axisrequestid.Valid && description.Valid && published_date.Valid && contributor.Valid && applicant_type.Valid && fees.Valid && bcgovcode.Valid && sitemap_pages.Valid && queuetype.Valid {
+			if _, ok := oiRecordsMap[int(foiministryrequestid.Int64)]; !ok {
+				oiRecordsMap[int(foiministryrequestid.Int64)] = &OpenInfoRecord{
+					Openinfoid:           int(openinfoid.Int64),
+					Foiministryrequestid: int(foiministryrequestid.Int64),
+					Axisrequestid:        axisrequestid.String,
+					Description:          description.String,
+					Published_date:       published_date.String,
+					Contributor:          contributor.String,
+					Applicant_type:       applicant_type.String,
+					Fees:                 fees.Float64,
+					BCgovcode:            bcgovcode.String,
+					Sitemap_pages:        sitemap_pages.String,
+					Type:                 queuetype.String,
+				}
+			}
+
+			if additionalfileid.Valid && filename.Valid && s3uripath.Valid {
+				oiRecordsMap[int(foiministryrequestid.Int64)].Additionalfiles = append(oiRecordsMap[int(foiministryrequestid.Int64)].Additionalfiles, AdditionalFile{
+					Additionalfileid: int(additionalfileid.Int64),
+					Filename:         filename.String,
+					S3uripath:        s3uripath.String,
+				})
+			}
+		}
+
+		// records = append(records, record)
+		// fmt.Printf("ID: %s, Description: %s, Published Date: %s, Contributor: %s, Applicant Type: %s, Fees: %v\n", record.Axisrequestid, record.Description, record.Published_date, record.Contributor, record.Applicant_type, record.Fees)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return records, fmt.Errorf("failed to retrieve query result: %w", err)
+	}
+
+	for _, record := range oiRecordsMap {
+		records = append(records, *record)
+	}
+
+	return records, nil
+}
+
+func GetOIRecordsForPublishing(db *sql.DB) ([]OpenInfoRecord, error) {
+	var records []OpenInfoRecord
+	var record OpenInfoRecord
+
+	qry := fmt.Sprintf(`
+		SELECT
+			oi.foiopeninforequestid,
+			mr.foiministryrequestid,
+			mr.axisrequestid,
+			mr.description,
+			oi.publicationdate,
+			pa.name as contributor,
+			ac.name as applicant_type,
+			COALESCE((fee.feedata->>'amountpaid')::Numeric, 0) as fees,
+			LOWER(pa.bcgovcode) as bcgovcode
+		FROM public."FOIMinistryRequests" mr
+		INNER JOIN public."FOIRequests" r on mr.foirequest_id = r.foirequestid and mr.foirequestversion_id = r.version
+		INNER JOIN public."ProgramAreas" pa on mr.programareaid = pa.programareaid
+		INNER JOIN public."ApplicantCategories" ac on r.applicantcategoryid = ac.applicantcategoryid
+		LEFT JOIN (
+			SELECT ministryrequestid, MAX(version) as max_version
+			FROM public."FOIRequestCFRFees"
+			GROUP BY ministryrequestid
+		) latest_payment on mr.foiministryrequestid = latest_payment.ministryrequestid
+		LEFT JOIN public."FOIRequestCFRFees" fee on mr.foiministryrequestid = fee.ministryrequestid 
+			and latest_payment.max_version = fee.version and mr.version = fee.ministryrequestversion
+		INNER JOIN public."FOIOpenInformationRequests" oi on mr.foiministryrequestid = oi.foiministryrequest_id and oi.isactive = TRUE
+		WHERE oi.processingstatus = '%s' and mr.isactive = TRUE
+	`, openstatus_ready)
+
+	rows, err := db.Query(qry)
+	if err != nil {
+		return records, fmt.Errorf("query failed: %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(
+			&record.Openinfoid,
+			&record.Foiministryrequestid,
+			&record.Axisrequestid,
+			&record.Description,
+			&record.Published_date,
+			&record.Contributor,
+			&record.Applicant_type,
+			&record.Fees,
+			&record.BCgovcode)
+		if err != nil {
+			return records, fmt.Errorf("failed to retrieve query result: %w", err)
+		}
+		records = append(records, record)
+		fmt.Printf("ID: %s, Description: %s, Published Date: %s, Contributor: %s, Applicant Type: %s, Fees: %v\n", record.Axisrequestid, record.Description, record.Published_date, record.Contributor, record.Applicant_type, record.Fees)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return records, fmt.Errorf("failed to retrieve query result: %w", err)
+	}
+
+	return records, nil
+}
+
+func GetOIRecordsForUnpublishing(db *sql.DB) ([]OpenInfoRecord, error) {
+	var records []OpenInfoRecord
+	var record OpenInfoRecord
+
+	qry := fmt.Sprintf(`
+		SELECT
+			oi.foiopeninforequestid,
+			mr.axisrequestid,
+			COALESCE(oi.sitemap_pages, '') as sitemap_pages,
+			'unpublish' as type
+		FROM public."FOIOpenInformationRequests" oi
+		INNER JOIN public."FOIMinistryRequests" mr on oi.foiministryrequest_id = mr.foiministryrequestid and mr.isactive = TRUE
+		INNER JOIN public."OpenInformationStatuses" oistatus on mr.oistatusid = oistatus.oistatusid
+		INNER JOIN public."OpenInfoPublicationStatuses" oirequesttype on oi.oipublicationstatus_id = oirequesttype.oipublicationstatusid
+		WHERE oirequesttype.name = '%s' and oi.processingstatus != '%s' and oi.isactive = TRUE
+	`, oirequesttype_unpublish, openstatus_unpublish)
+
+	rows, err := db.Query(qry)
+	if err != nil {
+		return records, fmt.Errorf("query failed: %w", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(
+			&record.Openinfoid,
+			&record.Axisrequestid,
+			&record.Sitemap_pages,
+			&record.Type,
+		)
+		if err != nil {
+			return records, fmt.Errorf("failed to retrieve query result: %w", err)
+		}
+		records = append(records, record)
+		fmt.Printf("ID: %s, Description: %s, Published Date: %s, Contributor: %s, Applicant Type: %s, Fees: %v\n", record.Axisrequestid, record.Description, record.Published_date, record.Contributor, record.Applicant_type, record.Fees)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return records, fmt.Errorf("failed to retrieve query result: %w", err)
+	}
+
+	return records, nil
+}
+
+func UpdateOIRecordState(db *sql.DB, foiministryrequestid int, publishingstatus string, message string, sitemap_pages string) error {
+
+	// Begin a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Error beginning transaction: %v", err)
+	}
+
+	// // Retrieve oipublicationstatus_id based on oistatus
+	// var oistatusid int
+	// err = tx.QueryRow(`SELECT oistatusid FROM public."OpenInformationStatuses" WHERE name = $1`, state).Scan(&oistatusid)
+	// if err != nil {
+	// 	tx.Rollback()
+	// 	log.Fatalf("Error retrieving oistatusid: %v", err)
+	// }
+
+	// Step 1: Set previous versions' isactive to false
+	_, err = tx.Exec(`UPDATE public."FOIOpenInformationRequests" SET isactive = false, updated_at = $2, updatedby = 'publishingservice' WHERE foiministryrequest_id = $1 AND isactive = true`, foiministryrequestid, time.Now())
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error updating previous versions: %v", err)
+	}
+
+	// Step 2: Insert a new version of the record
+	_, err = tx.Exec(`
+        INSERT INTO public."FOIOpenInformationRequests" (version, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, isactive, copyrightsevered, created_at, updated_at, createdby, updatedby, processingstatus, processingmessage, sitemap_pages)
+        SELECT version + 1, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, true, copyrightsevered, $2, NULL, 'publishingservice', NULL, $3, $4, $5
+        FROM public."FOIOpenInformationRequests"
+        WHERE foiministryrequest_id = $1 AND isactive = false
+        ORDER BY version DESC
+        LIMIT 1
+    `, foiministryrequestid, time.Now(), publishingstatus, message, sitemap_pages)
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error inserting new version for sitemaps: %v", err)
+	}
+
+	// Commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("Error committing transaction: %v", err)
+	}
+
+	return err
+}
+
+func LogError(db *sql.DB, foiministryrequestid int, publishingstatus string, message string) error {
+
+	// Begin a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Error beginning transaction: %v", err)
+	}
+
+	// Step 1: Set previous versions' isactive to false
+	_, err = tx.Exec(`UPDATE public."FOIOpenInformationRequests" SET isactive = false, updated_at = $2, updatedby = 'publishingservice' WHERE foiministryrequest_id = $1 AND isactive = true`, foiministryrequestid, time.Now())
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error updating previous versions: %v", err)
+	}
+
+	// Step 2: Insert a new version of the record
+	_, err = tx.Exec(`
+        INSERT INTO public."FOIOpenInformationRequests" (version, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, isactive, copyrightsevered, created_at, updated_at, createdby, updatedby, processingstatus, processingmessage, sitemap_pages)
+        SELECT version + 1, foiministryrequest_id, foiministryrequestversion_id, oipublicationstatus_id, oiexemption_id, oiassignedto, oiexemptionapproved, pagereference, iaorationale, oifeedback, publicationdate, true, copyrightsevered, $2, NULL, 'publishingservice', NULL, $3, $4, sitemap_pages
+        FROM public."FOIOpenInformationRequests"
+        WHERE foiministryrequest_id = $1 AND isactive = false
+        ORDER BY version DESC
+        LIMIT 1
+    `, foiministryrequestid, time.Now(), publishingstatus, message)
+	if err != nil {
+		tx.Rollback()
+		log.Fatalf("Error inserting error message: %v", err)
+	}
+
+	// Commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("Error committing transaction: %v", err)
+	}
+
+	return err
+}
