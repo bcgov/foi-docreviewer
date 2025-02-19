f09ed48b575cd713233e112004af4f8ad68cf229
Abin Antony <abin.antony@aot-technologies.com>
2025-02-19 11:06:47 -0800

    hotfix for Section Stamp

--- web/src/components/FOI/Home/Redlining.js
+++ web/src/components/FOI/Home/Redlining.js
@@ -929,8 +929,7 @@ const Redlining = React.forwardRef(
         if (info['source'] === 'redactionApplied') {
           annotations.forEach((annotation) => {
             if(annotation.Subject == "Free Text"){
-              docInstance?.Core?.annotationManager.addAnnotation(annotation);
-              docInstance?.Core?.annotationManager.addAnnotation(annotation);
+              docInstance?.Core?.annotationManager.addAnnotation(annotation);              
             }
           });
         }
