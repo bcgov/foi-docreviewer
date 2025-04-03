20a198b45724b04cbdafc43637bac7e49a660a4d
unknown <nicholas.kan@aot-technologies.com>
2025-04-03 09:41:09 -0700

    add missing constants file

--- web/src/constants/constants.ts
+++ web/src/constants/constants.ts
@@ -21,3 +21,5 @@ export const REDACTION_SELECT_LIMIT = window._env_?.REACT_APP_REDACTION_SELECT_L
 export const BIG_HTTP_GET_TIMEOUT = window._env_?.REACT_APP_BIG_HTTP_GET_TIMEOUT ?? process.env.REACT_APP_BIG_HTTP_GET_TIMEOUT ?? 300000;
 export const REDLINE_OPACITY = window._env_?.REACT_APP_REDLINE_OPACITY ?? process.env.REACT_APP_REDLINE_OPACITY ?? 0.5;
 export const REDACTION_SECTION_BUFFER = window._env_?.REACT_APP_REDACTION_SECTION_BUFFER ?? process.env.REACT_APP_REDACTION_SECTION_BUFFER ?? 3;
+export const PII_CATEGORIES = window._env_?.REACT_APP_PII_CATEGORIES ?? process.env.REACT_APP_PII_CATEGORIES ?? 'Person,Email';
+export const PII_BLACKLIST = window._env_?.REACT_APP_PII_BLACKLIST ?? process.env.REACT_APP_PII_BLACKLIST ?? '@gov.bc.ca';
