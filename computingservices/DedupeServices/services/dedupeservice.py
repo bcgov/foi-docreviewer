577cb092d2bbb59bcdb0f04274d5707f1a77debf
Aparna <aparna.s@aot-technologies.com>
2025-05-23 13:12:15 -0700

    Api fix and log added

--- computingservices/DedupeServices/services/dedupeservice.py
+++ computingservices/DedupeServices/services/dedupeservice.py
@@ -29,7 +29,7 @@ def processmessage(message):
         #if not _incompatible:
             pagecalculatormessage = documentspagecalculatorproducerservice().createpagecalculatorproducermessage(message, _pagecount)
             pagecalculatorjobid = pagecalculatorjobstart(pagecalculatormessage)
-            print("Pushed to Page Calculator Stream!!!")
+            print("Pushed to Page Calculator Stream!!!", pagecalculatormessage)
             documentspagecalculatorproducerservice().producepagecalculatorevent(pagecalculatormessage, _pagecount, pagecalculatorjobid)
     except(Exception) as error:
         print("Exception while processing redis message, func processmessage(p3), Error : {0} ".format(error))
