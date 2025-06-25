d3dafc155c32cc383a948da4a9cc885ee7af4695
Abin Antony <abin.antony@aot-technologies.com>
2025-06-25 11:16:00 -0700

    #FOIMOD-4093 IDIR detect api Auth headers

--- api/reviewer_api/resources/IDIRService.py
+++ api/reviewer_api/resources/IDIRService.py
@@ -1,11 +1,20 @@
 from flask import request, jsonify
 from flask_restx import Resource, Namespace
 from reviewer_api.services.bcgovidirservice import bcgovidirservice
+from reviewer_api.auth import auth, AuthHelper
+from reviewer_api.tracer import Tracer
+from reviewer_api.utils.util import  cors_preflight, allowedorigins, getrequiredmemberships
+from flask_cors import cross_origin
 
 API = Namespace('bcgovidir', description='BCGovIDIR operations')
-
+TRACER = Tracer.get_instance()
 @API.route('/bcgovidir/check')
 class BCGovIDIRCheck(Resource):
+    
+    @TRACER.trace()
+    @cross_origin(origins=allowedorigins())
+    @auth.require
+    @auth.ismemberofgroups(getrequiredmemberships())
     def post(self):
         """
         Accepts a JSON array of sAMAccountName strings and returns matching records.
