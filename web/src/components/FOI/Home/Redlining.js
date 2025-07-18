2cfc301a008fb2c9a8015703a4ddccb6a60e4ad6
Abin Antony <abin.antony@aot-technologies.com>
2025-07-18 16:13:58 -0700

    #FOIMOD-3995 added Mssing author info

--- web/src/components/FOI/Home/Redlining.js
+++ web/src/components/FOI/Home/Redlining.js
@@ -890,6 +890,7 @@ const Redlining = React.forwardRef(
                                     Height: textQuad.x2 - textQuad.x1,
                                     Color: new annots.Color(255, 205, 69, 1),
                                     Quads: [textQuad],
+                                    Author: "PIIDetection"
                                   });
 
                                   annot.setCustomData("PIIDetection", true);
