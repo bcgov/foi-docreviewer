1fbd0f8fc4490e92d3f9e632e906a91e197d01eb
Milos Despotovic <milos.despotovic@gov.bc.ca>
2025-12-18 18:02:52 -0800

    Create temp folder for pdfcpu

--- computingservices/CompressionServices/main.go
+++ computingservices/CompressionServices/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	//cfg := utils.LoadConfig()
+	// create temp folder for pdfcpu
+	configDir := "/tmp/pdfcpu_config"
+	os.MkdirAll(configDir, 0o777)
+	// set XDG_CONFIG_HOME, which pdfcpu uses
+	os.Setenv("XDG_CONFIG_HOME", configDir)
 	consumerID := flag.String("consumer-id", "$", "ID of the consumer")
 	startFrom := flag.String("start-from", "$", "Where to start from in stream: 0 or $")
 	// Parse CLI flags
