98ced656b4a98ae8ba61eee281c4c50655759265
Milos Despotovic <milos.despotovic@gov.bc.ca>
2026-01-05 12:01:13 -0800

    Remove print

--- computingservices/CompressionServices/main.go
+++ computingservices/CompressionServices/main.go
@@ -12,9 +12,7 @@ func main() {
 	configDir := "/tmp/pdfcpu_config"
 	os.MkdirAll(configDir, 0o777)
 	// set XDG_CONFIG_HOME, which pdfcpu uses
-	// os.Setenv("XDG_CONFIG_HOME", configDir)
-	fmt.Printf("XDG_CONFIG_HOME: %s, Start from: %s\n", os.Getenv("XDG_CONFIG_HOME"))
-
+	os.Setenv("XDG_CONFIG_HOME", configDir)
 	consumerID := flag.String("consumer-id", "$", "ID of the consumer")
 	startFrom := flag.String("start-from", "$", "Where to start from in stream: 0 or $")
 	// Parse CLI flags
